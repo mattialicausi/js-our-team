beb8f3bfe22697c88f550d9d39a6086387b6a098
mattialicausi <licausivitomattia@gmail.com>
2022-10-20 13:24:18 +0200

    creazione repo

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede e stilarli a vostro gusto :faccia_leggermente_sorridente:
+BONUS 3:
+Aggiungere attraverso un form un membro al team
+*/
